49e7d300d3fe0d41846eb5a2da74bfa12caa659e
Merijn Post <merijnpost2006@gmail.com>
2022-09-20 11:33:16 +0200

    add a I64 v2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 fn main() {
-<<<<<<< HEAD
     let x = String::from("12");
     let c = "12";
     let y: i64 = 673778734768;
-=======
-    let c = String::from("12");
-    let c = "14";
->>>>>>> 5489a2c6475dd1453ddad24a553da9871c25c7bc
 
     println!("c = {} + 1", c);
     println!("Hello world + {}", x);
