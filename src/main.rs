5be4f74d4e668fdefe7faca432e3604607d9e179
Merijn Post <merijnpost2006@gmail.com>
2022-09-26 12:24:43 +0200

    all important types of variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 fn main() {
-    let x = String::from("12");
-    let c = "12";
-    let y: i64 = 673778734768;
+    //let x: i8 = 10;
+    //println!("{}", x);
 
-    println!("c = {} + 1", c);
-    println!("Hello world + {}", x);
-    println!("y = {}", y)
+    // let _y: u8 = 10;
+
+    //let decimial = 02_55;
+    //let hex = 0xff;
+    //let octal = 0o377;
+    //let binary = 0b1111_1111;
+
+    //println!("{}", decimial);
+    //println!("{}", hex);
+    //println!("{}", octal);
+    //println!("{}", binary);
+
+    //let byte = b'A';
+    //println!("{}", byte);
+
+    //let x = 2.0; //f64 default because on modern CPUs is roughly the same speed as f32
+    //let y: f32 = 1.0;
+    //let t = true;
+    //let f: bool = false;
+
+    //let c = 'c';
+
+    //println!("{}", c);
+
+    // +, -, *, / and %
+
+    //let a = 10;
+    //let b = 4;
+
+    //let remainder = a % b;
+    //println!("{}", remainder);
 
 }
