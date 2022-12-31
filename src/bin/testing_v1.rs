4e631b7e6225eb175315b7606e1814c5bde37c9c
Merijn Post <merijnpost2006@gmail.com>
2022-12-31 16:11:04 +0100

    getting back into rust

---
+++ src/bin/testing_v1.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+fn main (){
+    println!("A basic calculator program, you can only use whole numbers and it will only display whole numbers");
+
+    println!("Input number 1.");
+
+    let mut num_1 = String::new();
+
+    io::stdin()
+        .read_line(&mut num_1)
+        .expect("failed to read line");
+    
+    println!("Your first number is: {num_1}");
+
+    println!("Input number 2.");
+
+    let mut num_2 = String::new();
+
+    io::stdin()
+        .read_line(&mut num_2)
+        .expect("failed to read line");
+
+    println!("Your second number is: {num_2}");
+
+    println!("Put in operation.");
+
+    let mut operation = String::new();
+
+    io::stdin()
+        .read_line(&mut operation)
+        .expect("failed to read line");
+
+    println!("Your operation is: {operation}");
+
+    let x: i32 = num_1.trim().parse().unwrap();
+
+    let y: i32 = num_2.trim().parse().unwrap();
+
+    let answer = match operation.trim() {
+        "+" => x + y,
+        "-" => x - y,
+        "*" => x * y,
+        "/" => x / y,
+        _ => {
+            eprintln!("Wrong operation.");
+            std::process::exit(1)
+        }
+    };
+
+    println!("The answer is: {answer}");
+}
